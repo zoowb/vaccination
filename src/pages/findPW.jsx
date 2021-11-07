542a50e429e9baa05e9884030da21c15b906b311
ssu00 <rachel4535@naver.com>
2021-11-07 13:23:23 +0900

    ⚡️ added findID, findPW page

---
+++ src/pages/findPW.jsx
@@ -0,0 +1,32 @@
+import WholeScreen from "../components/wholeScreen";
+import WhiteScreen from "../components/auth/whiteScreen";
+import BlueBtn from "../components/auth/blueBtn";
+import InputBox from "../components/auth/inputBox";
+import LinkBtnSet from "../components/auth/linkBtn";
+import "./find.css";
+const FindPW = () => {
+  return (
+    <section className="find">
+      <WholeScreen>
+        <WhiteScreen>
+          <div className="authImg" />
+          <div className="content">
+            <h1 className="title">비밀번호 찾기</h1>
+            <InputBox type={"text"} placeholder={"이름"} />
+            <InputBox type={"number"} placeholder={"주민번호"} />
+            <BlueBtn text={"비밀번호 찾기"}></BlueBtn>
+            <LinkBtnSet
+              text1={"로그인"}
+              url1={"/login"}
+              text2={"회원가입"}
+              url2={"/signup"}
+              text3={"아이디 찾기"}
+              url3={"/findID"}
+            />
+          </div>
+        </WhiteScreen>
+      </WholeScreen>
+    </section>
+  );
+};
+export default FindPW;
