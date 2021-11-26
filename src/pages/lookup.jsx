3fb2de47bbb6d3598dd8a69459b52f5ea3767e79
zoowb <zoowb0829@gmail.com>
2021-11-27 03:50:37 +0900

    lookup change

--- src/pages/lookup.jsx
+++ src/pages/lookup.jsx
@@ -3,25 +3,8 @@ import {WholeScreenWithHeader} from "../components/wholeScreen";
 import Vaccineicon from "./image/vaccineicon.png";
 import "./lookup.css";
 
-const TARGETS = [
-  { value: 1, name: "18~49세 (2003년생~1972년생) 미접종자" },
-  { value: 2, name: "50세 이상 (1973년생 이상) 미접종자" },
-];
 const LOCATIONS1 = [{ value: 1, name: "서울특별시" }];
 const LOCATIONS2 = [{ value: 1, name: "강남구" }];
-const LOCATIONS3 = [{ value: 1, name: "수서동" }];
-
-const SelectBoxT = (props) => {
-  return (
-    <select className="lookuptargetBox">
-      {props.inform.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.name}
-        </option>
-      ))}
-    </select>
-  );
-};
 
 const SelectBoxL = (props) => {
   return (
@@ -153,69 +136,66 @@ const HospitalLookDetail = ({
 const Lookup = () => {
   return (
     <WholeScreenWithHeader>
-      <div className="lookuptopside">
-        <div className="biglayout">
-          <div className="titlelu">대상</div>
-          <SelectBoxT inform={TARGETS}></SelectBoxT>
-          <div className={"checkset"}>
-            <input className={"checkbox"} type="checkbox" />
-            <div className={"checkboxtext"}>예약불가 포함</div>
+      <div className="lookup">
+        <div className="lookuptopside">
+          <div className="biglayout">
+            <div className="titlelu">지역</div>
+            <SelectBoxL inform={LOCATIONS1}></SelectBoxL>
+            <SelectBoxL inform={LOCATIONS2}></SelectBoxL>
+            <div className={"checkset"}>
+              <input className={"checkbox"} type="checkbox" />
+              <div className={"checkboxtext"}>예약불가 포함</div>
+            </div>
+            <div className={"btnset"}>
+              <button type="button" className={"bluebtn"}>
+                {" "}
+                결과보기{" "}
+              </button>
+              <button type="button" className={"redbtn"}>
+                {" "}
+                초기화{" "}
+              </button>
+            </div>
           </div>
         </div>
-        <div className="biglayout">
-          <div className="titlelu">거주지</div>
-          <SelectBoxL inform={LOCATIONS1}></SelectBoxL>
-          <SelectBoxL inform={LOCATIONS2}></SelectBoxL>
-          <SelectBoxL inform={LOCATIONS3}></SelectBoxL>
-          <div className={"btnset"}>
-            <button type="button" className={"bluebtn"}>
-              {" "}
-              결과보기{" "}
-            </button>
-            <button type="button" className={"redbtn"}>
-              {" "}
-              초기화{" "}
-            </button>
+        <hr className={"lookuphr"}></hr>
+        <Search />
+        <div className="lookupsearchnum">검색결과 총 24개</div>
+        <div className="lookupbottomside">
+          <div className="hlist">
+            <Hospitallist
+              address={"서울특별시 강남구 세곡동"}
+              hname={"청담오라클피부과성형외과의원"}
+            />
+            <Hospitallist
+              address={"서울특별시 강남구 세곡동"}
+              hname={"청담오라클피부과성형외과의원"}
+            />
+            <Hospitallist
+              address={"서울특별시 강남구 세곡동"}
+              hname={"청담오라클피부과성형외과의원"}
+            />
+            <Hospitallist
+              address={"서울특별시 강남구 세곡동"}
+              hname={"청담오라클피부과성형외과의원"}
+            />
+            <Hospitallist
+              address={"서울특별시 강남구 세곡동"}
+              hname={"청담오라클피부과성형외과의원"}
+            />
           </div>
-        </div>
-      </div>
-      <hr className={"lookuphr"}></hr>
-      <Search />
-      <div className="lookupsearchnum">검색결과 총 24개</div>
-      <div className="lookupbottomside">
-        <div className="hlist">
-          <Hospitallist
-            address={"서울특별시 강남구 세곡동"}
+          <HospitalLookDetail
             hname={"청담오라클피부과성형외과의원"}
-          />
-          <Hospitallist
-            address={"서울특별시 강남구 세곡동"}
-            hname={"청담오라클피부과성형외과의원"}
-          />
-          <Hospitallist
-            address={"서울특별시 강남구 세곡동"}
-            hname={"청담오라클피부과성형외과의원"}
-          />
-          <Hospitallist
-            address={"서울특별시 강남구 세곡동"}
-            hname={"청담오라클피부과성형외과의원"}
-          />
-          <Hospitallist
-            address={"서울특별시 강남구 세곡동"}
-            hname={"청담오라클피부과성형외과의원"}
+            address={"서울특별시 강남구 광평로 281, (수서동, 2층일부)"}
+            vacnum={"8"}
+            phone={"02-6181-3855"}
+            time1={"08:00 - 18:30"}
+            time2={"미운영"}
+            time3={"미운영"}
+            time4={"미운영"}
+            etc={"평일점심 12:00 - 13:00"}
           />
         </div>
-        <HospitalLookDetail
-          hname={"청담오라클피부과성형외과의원"}
-          address={"서울특별시 강남구 광평로 281, (수서동, 2층일부)"}
-          vacnum={"8"}
-          phone={"02-6181-3855"}
-          time1={"08:00 - 18:30"}
-          time2={"미운영"}
-          time3={"미운영"}
-          time4={"미운영"}
-          etc={"평일점심 12:00 - 13:00"}
-        />
       </div>
     </WholeScreenWithHeader>
   );
