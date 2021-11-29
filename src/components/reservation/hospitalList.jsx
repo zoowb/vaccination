a8fe574ea8e308dea8ce1b20dfaebaf24df133c9
ssu00 <rachel4535@naver.com>
2021-11-29 12:37:23 +0900

    ⚡️ edited tmap for no-show,linked with server,etc

--- src/components/reservation/hospitalList.jsx
+++ src/components/reservation/hospitalList.jsx
@@ -1,72 +1,80 @@
 import React, { useState } from "react";
 import CheckBox from "./checkBox";
-import "./hospitalList.css"
+import "./hospitalList.css";
 
+const HospitalList = ({
+  name,
+  vname1,
+  vnum1,
+  vname2,
+  vnum2,
+  vname3,
+  vnum3,
+  vname4,
+  vnum4,
+  time,
+}) => {
+  const [modalOn, setModalOn] = useState(false);
+  const onOpenModal = () => {
+    setModalOn(!modalOn);
+  };
 
-
-const HospitalList = ( {name, vname1, vnum1, vname2, vnum2, vname3, vnum3, vname4, vnum4, time} ) => {
-    
-    const [modalOn, setModalOn] = useState(false);
-    const onOpenModal = () =>{
-        setModalOn(!modalOn);
-    }
-
-    const Modal = () => {
-        return (
-            <div className = "noshowmodal">
-                <div className = "bg"></div>
-                <div className = "modalBox">
-                        <div className = "modaltext">접종할 백신을 선택해주세요.</div>
-                        <div className = "modalsm">
-                        <div className={"checkset"}>
-                            <input className={"checkbox"} type = "checkbox" />
-                            <div className={"checkboxtext"}>{vname1}</div>
-                            <input className={"checkbox"} type = "checkbox" />
-                            <div className={"checkboxtext"}>{vname2}</div>
-                            <input className={"checkbox"} type = "checkbox" />
-                            <div className={"checkboxtext"}>{vname3}</div>
-                        </div>
-                        </div>
-                        <button className = "closeBtn" onClick = {onOpenModal}>
-                            <div className = "btnText">선택완료 </div>
-                        </button>
-                </div>
+  const Modal = () => {
+    return (
+      <div className="noshowmodal">
+        <div className="bg"></div>
+        <div className="modalBox">
+          <div className="modaltext">접종할 백신을 선택해주세요.</div>
+          <div className="modalsm">
+            <div className={"checkset"}>
+              <input className={"checkbox"} type="checkbox" />
+              <div className={"checkboxtext"}>{vname1}</div>
+              <input className={"checkbox"} type="checkbox" />
+              <div className={"checkboxtext"}>{vname2}</div>
+              <input className={"checkbox"} type="checkbox" />
+              <div className={"checkboxtext"}>{vname3}</div>
             </div>
-        )
-    }
-
-    return(
-        <button type="button" className={"hospitallistBigBox"}>
-            <section className={"listBox"}>
-                <div>
-                    <div className={"hospitalName"}>{name}</div>
-                    <div className={"vaccine"}>
-                        <div className={"vaccineName"}>{vname1}</div>
-                        &nbsp;
-                        <div className={"vaccineNum"}>{vnum1}</div>
-                        &nbsp;
-                        <div className={"vaccineName"}>{vname2}</div>
-                        &nbsp;
-                        <div className={"vaccineNum"}>{vnum2}</div>
-                        &nbsp;
-                        <div className={"vaccineName"}>{vname3}</div>
-                        &nbsp;
-                        <div className={"vaccineNum"}>{vnum3}</div>
-                        &nbsp;
-                        <div className={"vaccineName"}>{vname4}</div>
-                        &nbsp;
-                        <div className={"vaccineNum"}>{vnum4}</div>
-                    </div>
-                    <div className={"hospitalTime"}>{time}</div>
-                </div>
-                
-                <button type="button" className="redButton" onClick={onOpenModal}>
-                    <span className="redButtonText">예약하기</span>
-                    {modalOn && <Modal onOpenModal={onOpenModal}></Modal>}
-                </button>
-            </section>
-        </button>
+          </div>
+          <button className="closeBtn" onClick={onOpenModal}>
+            <div className="btnText">선택완료 </div>
+          </button>
+        </div>
+      </div>
     );
+  };
+
+  return (
+    <button type="button" className={"hospitallistBigBox"}>
+      {modalOn && <Modal onOpenModal={onOpenModal}></Modal>}
+      <section className={"listBox"}>
+        <div>
+          <div className={"hospitalName"}>{name}</div>
+          <div className={"vaccine"}>
+            <div className={"vaccineName"}>{vname1}</div>
+            &nbsp;
+            <div className={"vaccineNum"}>{vnum1}</div>
+            &nbsp;
+            <div className={"vaccineName"}>{vname2}</div>
+            &nbsp;
+            <div className={"vaccineNum"}>{vnum2}</div>
+            &nbsp;
+            <div className={"vaccineName"}>{vname3}</div>
+            &nbsp;
+            <div className={"vaccineNum"}>{vnum3}</div>
+            &nbsp;
+            <div className={"vaccineName"}>{vname4}</div>
+            &nbsp;
+            <div className={"vaccineNum"}>{vnum4}</div>
+          </div>
+          <div className={"hospitalTime"}>{time}</div>
+        </div>
+
+        <button type="button" className="redButton" onClick={onOpenModal}>
+          <span className="redButtonText">예약하기</span>
+        </button>
+      </section>
+    </button>
+  );
 };
 
 export default HospitalList;
--- src/pages/reservationNoShow.jsx
+++ src/pages/reservationNoShow.jsx
@@ -1,121 +1,173 @@
 import Title from "../components/reservation/title";
-import {WholeScreenWithHeader} from "../components/wholeScreen";
+import { WholeScreenWithHeader } from "../components/wholeScreen";
 import CheckBox from "../components/reservation/checkBox";
 import HospitalList from "../components/reservation/hospitalList";
 import initTmap from "../components/tmapfornoshow";
-import "./reservationNoShow.css"
-import React, { useEffect } from 'react';
+import "./reservationNoShow.css";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
-const  ReservationNoShow = () => {
-    useEffect(() => {
-        initTmap(126.984895, 37.566369);
-      }, []);
+const ReservationNoShow = () => {
+  const [ssn, setSsn] = useState("");
+  let loc = {};
+  let arr = [];
+  let x = 0,
+    y = 0;
 
-    return (
-        <WholeScreenWithHeader>
-            <section className="noshow">
-                <section className="leftside">
-                <div className="head">
-                        <Title
-                            title={"잔여백신 조회"}
-                            subtitle={"집 근처의 잔여백신을 조회해보세요!"}
-                        />
-                </div>
-                <div className="checkbig">
-                    <CheckBox 
-                        name1 = {"화이자"}
-                        name2 = {"모더나"}
-                        name3 = {"아스트라제네카"}/>
-                </div>
-                <div className="hospitalListBox">
-                    <HospitalList
-                        name={"참좋은 병원"}
-                        vname1={"화이자"}
-                        vnum1={12}
-                        vname2={"모더나"}
-                        vnum2={12}
-                        vname3={"아스트라제네카"}
-                        vnum3={10}
-                        time={"운영시간: 10:00~18:00"}
-                    />
-                    <HospitalList
-                        name={"참좋은 병원"}
-                        vname1={"화이자"}
-                        vnum1={12}
-                        vname2={"모더나"}
-                        vnum2={12}
-                        vname3={"아스트라제네카"}
-                        vnum3={10}
-                        time={"운영시간: 10:00~18:00"}
-                    />
-                    <HospitalList
-                        name={"참좋은 병원"}
-                        vname1={"화이자"}
-                        vnum1={12}
-                        vname2={"모더나"}
-                        vnum2={12}
-                        vname3={"아스트라제네카"}
-                        vnum3={10}
-                        time={"운영시간: 10:00~18:00"}
-                    />
-                    <HospitalList
-                        name={"참좋은 병원"}
-                        vname1={"화이자"}
-                        vnum1={12}
-                        vname2={"모더나"}
-                        vnum2={12}
-                        vname3={"아스트라제네카"}
-                        vnum3={10}
-                        time={"운영시간: 10:00~18:00"}
-                    />
-                    <HospitalList
-                        name={"참좋은 병원"}
-                        vname1={"화이자"}
-                        vnum1={12}
-                        vname2={"모더나"}
-                        vnum2={12}
-                        vname3={"아스트라제네카"}
-                        vnum3={10}
-                        time={"운영시간: 10:00~18:00"}
-                    />
-                    <HospitalList
-                        name={"참좋은 병원"}
-                        vname1={"화이자"}
-                        vnum1={12}
-                        vname2={"모더나"}
-                        vnum2={12}
-                        vname3={"아스트라제네카"}
-                        vnum3={10}
-                        time={"운영시간: 10:00~18:00"}
-                    />
-                    <HospitalList
-                        name={"참좋은 병원"}
-                        vname1={"화이자"}
-                        vnum1={12}
-                        vname2={"모더나"}
-                        vnum2={12}
-                        vname3={"아스트라제네카"}
-                        vnum3={10}
-                        time={"운영시간: 10:00~18:00"}
-                    />
-                    <HospitalList
-                        name={"참좋은 병원"}
-                        vname1={"화이자"}
-                        vnum1={12}
-                        vname2={"모더나"}
-                        vnum2={12}
-                        vname3={"아스트라제네카"}
-                        vnum3={10}
-                        time={"운영시간: 10:00~18:00"}
-                    />
-                </div>
-                </section>
-                <div className="mapBox">
-                    <div className="mapMap" id="map_div"/>
-                </div>
-            </section>
-        </WholeScreenWithHeader>
-    )
-}
+  const getNoShow = () => {
+    const token = localStorage.getItem("accessToken");
+    axios
+      .post("/mypage/getinfo", { jwtToken: token })
+      .then((response) => {
+        console.log(response);
+        setSsn(response.data.Ssn);
+        x = response.data.x;
+        y = response.data.y;
+        loc = {};
+        loc.name = "현재 위치";
+        loc.x = x;
+        loc.y = y;
+        arr.push(loc);
+      })
+      .catch((e) => console.log(e));
+    axios
+      .post("/vaccine/index", { ssn: ssn, jwtToken: token })
+      .then((response) => {
+        console.log("vaccine==", response);
+        let length = response.data.hosList.length;
+        response.data.hosList.map((data, i) => {
+          axios
+            .post("/search/more", { idx: data.Hnumber, isHos: true })
+            .then((response) => {
+              loc = {};
+              loc.name = response.data.info.Hname;
+              loc.x = response.data.info.x;
+              loc.y = response.data.info.y;
+              arr.push(loc);
+              if (i == length - 1) {
+                initTmap(x, y, arr);
+              }
+            })
+            .catch((e) => console.log(e));
+        });
+      })
+      .catch((e) => console.log(e));
+  };
+
+  useEffect(() => {
+    getNoShow();
+  }, []);
+
+  useEffect(() => {
+    console.log(loc);
+  }, [loc]);
+
+  return (
+    <WholeScreenWithHeader>
+      <section className="noshow">
+        <section className="leftside">
+          <div className="head">
+            <Title
+              title={"잔여백신 조회"}
+              subtitle={"집 근처의 잔여백신을 조회해보세요!"}
+            />
+          </div>
+          <div className="checkbig">
+            <CheckBox
+              name1={"화이자"}
+              name2={"모더나"}
+              name3={"아스트라제네카"}
+            />
+          </div>
+          <div className="hospitalListBox">
+            <HospitalList
+              name={"참좋은 병원"}
+              vname1={"화이자"}
+              vnum1={12}
+              vname2={"모더나"}
+              vnum2={12}
+              vname3={"아스트라제네카"}
+              vnum3={10}
+              time={"운영시간: 10:00~18:00"}
+            />
+            <HospitalList
+              name={"참좋은 병원"}
+              vname1={"화이자"}
+              vnum1={12}
+              vname2={"모더나"}
+              vnum2={12}
+              vname3={"아스트라제네카"}
+              vnum3={10}
+              time={"운영시간: 10:00~18:00"}
+            />
+            <HospitalList
+              name={"참좋은 병원"}
+              vname1={"화이자"}
+              vnum1={12}
+              vname2={"모더나"}
+              vnum2={12}
+              vname3={"아스트라제네카"}
+              vnum3={10}
+              time={"운영시간: 10:00~18:00"}
+            />
+            <HospitalList
+              name={"참좋은 병원"}
+              vname1={"화이자"}
+              vnum1={12}
+              vname2={"모더나"}
+              vnum2={12}
+              vname3={"아스트라제네카"}
+              vnum3={10}
+              time={"운영시간: 10:00~18:00"}
+            />
+            <HospitalList
+              name={"참좋은 병원"}
+              vname1={"화이자"}
+              vnum1={12}
+              vname2={"모더나"}
+              vnum2={12}
+              vname3={"아스트라제네카"}
+              vnum3={10}
+              time={"운영시간: 10:00~18:00"}
+            />
+            <HospitalList
+              name={"참좋은 병원"}
+              vname1={"화이자"}
+              vnum1={12}
+              vname2={"모더나"}
+              vnum2={12}
+              vname3={"아스트라제네카"}
+              vnum3={10}
+              time={"운영시간: 10:00~18:00"}
+            />
+            <HospitalList
+              name={"참좋은 병원"}
+              vname1={"화이자"}
+              vnum1={12}
+              vname2={"모더나"}
+              vnum2={12}
+              vname3={"아스트라제네카"}
+              vnum3={10}
+              time={"운영시간: 10:00~18:00"}
+            />
+            <HospitalList
+              name={"참좋은 병원"}
+              vname1={"화이자"}
+              vnum1={12}
+              vname2={"모더나"}
+              vnum2={12}
+              vname3={"아스트라제네카"}
+              vnum3={10}
+              time={"운영시간: 10:00~18:00"}
+            />
+          </div>
+        </section>
+        <div className="mapBox">
+          <div className="mapMap" id="map_div" />
+        </div>
+      </section>
+    </WholeScreenWithHeader>
+  );
+};
 export default ReservationNoShow;
