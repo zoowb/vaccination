1a08c1a5888caa810629b932eb295fe1a745849e
ssu00 <rachel4535@naver.com>
2021-11-29 12:38:21 +0900

    💄 edited style (findID,findPW's result page)

--- src/pages/findPWResult.jsx
+++ src/pages/findPWResult.jsx
@@ -2,25 +2,41 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import WholeScreen from "../components/wholeScreen";
 import WhiteScreen from "../components/auth/whiteScreen";
-import BlueBtn from "../components/auth/blueBtn";
 import { useLocation } from "react-router-dom";
-import "./find.css";
+import "./findResult.css";
 const FindPWResult = () => {
   const navigate = useNavigate();
   const location = useLocation();
   return (
-    <section className="find">
+    <section className="findResult">
       <WholeScreen>
         <Link to={"/"}>
           <div className="logo" />
         </Link>
         <WhiteScreen>
+          <div className="authImg" />
           <div className="content">
             <h1 className="title">비밀번호 찾기</h1>
-            <h1>회원님의 비밀번호는 {location.state.pw}입니다.</h1>
-
-            <BlueBtn text={"로그인"} onClick={() => navigate("/login")} />
-            <BlueBtn text={"메인 화면으로"} onClick={() => navigate("/")} />
+            <h1 className="resultText">
+              회원님의 비밀번호는 <br />
+              <strong className="result">{location.state.pw}</strong>입니다.
+            </h1>
+            <div className="btnSet">
+              <button
+                type="button"
+                className={"blueBtn"}
+                onClick={() => navigate("/login")}
+              >
+                <span className="text">로그인</span>
+              </button>
+              <button
+                type="button"
+                className={"blueBtn"}
+                onClick={() => navigate("/")}
+              >
+                <span className="text">메인화면으로</span>
+              </button>
+            </div>
           </div>
         </WhiteScreen>
       </WholeScreen>
