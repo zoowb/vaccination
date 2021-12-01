0b491e9e161c02c75616c36906df35604cdb9b52
ssu00 <rachel4535@naver.com>
2021-12-01 18:17:50 +0900

    💄 added disabled btn for auth

--- src/components/auth/blueBtn.jsx
+++ src/components/auth/blueBtn.jsx
@@ -1,10 +1,19 @@
 import "./blueBtn.css";
-const BlueBtn = ({ text, onClick }) => {
+const BlueBtn = ({ text, onClick, disable }) => {
   return (
     <button
       type="button"
-      className={text == "회원가입" ? "auth-blueBtn2" : "auth-blueBtn"}
+      className={
+        text == "회원가입"
+          ? disable
+            ? "Dauth-blueBtn2"
+            : "auth-blueBtn2"
+          : disable
+          ? "Dauth-blueBtn"
+          : "auth-blueBtn"
+      }
       onClick={onClick}
+      disabled={disable ? disable : ""}
     >
       <span className="text">{text}</span>
     </button>
