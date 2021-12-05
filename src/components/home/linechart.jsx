264d90812325e18b46eb0fb65136f563ecf0d727
ssu00 <rachel4535@naver.com>
2021-12-05 23:06:36 +0900

    💄 edited line chart's style

--- src/components/home/linechart.jsx
+++ src/components/home/linechart.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Chart } from "react-google-charts";
 import axios from "axios";
 
-const LineChart = ({check}) => {
-  const [firstmonth , setFirstmonth] = useState([]);
-  const [donemonth , setDonemonth] = useState([]);
+const LineChart = ({ check }) => {
+  const [firstmonth, setFirstmonth] = useState([]);
+  const [donemonth, setDonemonth] = useState([]);
   const [firstday, setFirstday] = useState([]);
   const [doneday, setDoneday] = useState([]);
   let now = new Date();
@@ -12,54 +12,95 @@ const LineChart = ({check}) => {
   let todayDate = now.getDate();
 
   const getData = () => {
-    axios.post("/home/index")
-    .then((response)=>{
+    axios
+      .post("/home/index")
+      .then((response) => {
         setFirstmonth(response.data.byMonth_vac1);
         setDonemonth(response.data.byMonth_vac2);
         setFirstday(response.data.byDay_vac1);
-        setDoneday(response.data.byDay_vac2);      
-    })
-    .catch((e)=>{
-      console.log(e)
-    });
+        setDoneday(response.data.byDay_vac2);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getData();
   }, []);
 
   const Makemontharr = () => {
     const montharr = [["1", "1차 접종", "완전 접종"]];
-    if( donemonth.length != 0){
-      for( let i=0; i<firstmonth.length; i++){
-           montharr.push([`${(todayMonth-4)+i}월`, firstmonth[(firstmonth.length-5)+i].count, donemonth[(donemonth.length-5)+i].count]);
+    if (donemonth.length != 0) {
+      for (let i = 0; i < firstmonth.length; i++) {
+        montharr.push([
+          `${todayMonth - 4 + i}월`,
+          firstmonth[firstmonth.length - 5 + i].count,
+          donemonth[donemonth.length - 5 + i].count,
+        ]);
       }
     }
-    return <Chart width={"550px"} height={"350px"} chartType="LineChart" loader={<div>Loading Chart</div>} data={montharr}
-    options={{ colors: ['#5064C5', '#CD2A82'], chartArea: { width: "80%", height: "80%" },}}
-    />
-  }
+    return (
+      <Chart
+        width={"600px"}
+        height={"350px"}
+        chartType="LineChart"
+        loader={<div>Loading Chart</div>}
+        data={montharr}
+        options={{
+          colors: ["#5064C5", "#CD2A82"],
+          chartArea: { width: "65%", height: "65%" },
+          hAxis: {
+            title: "날짜",
+          },
+          vAxis: {
+            title: "접종자 수",
+          },
+        }}
+      />
+    );
+  };
 
   const Makedayarr = () => {
-    const dayarr = [["1", "1차 접종", "완전 접종"]];
-    if( doneday.length != 0){
-      for(let i=firstday.length-1; i>=0; i--){
-        if(todayDate==firstday[i].day){
-              for(let j=i-5; j<=i; j++){
-                dayarr.push([`${firstday[j].month}/${(firstday[j].day)}`, firstday[j].count, doneday[j].count]);
-              }
-              break;
+    const dayarr = [["1", "1차접종", "완전접종"]];
+    if (doneday.length != 0) {
+      for (let i = firstday.length - 1; i >= 0; i--) {
+        if (todayDate == firstday[i].day) {
+          for (let j = i - 5; j <= i; j++) {
+            dayarr.push([
+              `${firstday[j].month}/${firstday[j].day}`,
+              firstday[j].count,
+              doneday[j].count,
+            ]);
           }
+          break;
+        }
       }
     }
-    return <Chart width={"550px"} height={"350px"} chartType="LineChart" loader={<div>Loading Chart</div>} data={dayarr}
-    options={{ colors: ['#5064C5', '#CD2A82'], chartArea: { width: "80%", height: "80%" },}}
-    />
-  }
+    return (
+      <Chart
+        width={"600px"}
+        height={"350px"}
+        chartType="LineChart"
+        loader={<div>Loading Chart</div>}
+        data={dayarr}
+        options={{
+          colors: ["#5064C5", "#CD2A82"],
+          chartArea: { width: "65%", height: "65%" },
+          hAxis: {
+            title: "날짜",
+          },
+          vAxis: {
+            title: "접종자 수",
+          },
+        }}
+      />
+    );
+  };
 
   return (
     <div className="chart">
-      {check && firstmonth.length!=0 ? (
+      {check && firstmonth.length != 0 ? (
         <>
           <Makemontharr />
         </>
@@ -67,7 +108,6 @@ const LineChart = ({check}) => {
         <>
           <Makedayarr />
         </>
-
       )}
     </div>
   );
